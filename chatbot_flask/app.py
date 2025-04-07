5ffa483a4580a047b688f475ec8e5641999868df
Kunj-Mori <kunjmori18@gmail.com>
2025-04-07 20:08:50 +0530

    Initial commit

--- chatbot_flask/app.py
+++ chatbot_flask/app.py
@@ -124,4 +124,5 @@ def index():
     return render_template("chat.html", chat_history=chat_history)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
